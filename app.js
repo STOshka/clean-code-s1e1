5cfe934836c3413dd5b67b1c390153a43931b35f
STOshka <34401821+STOshka@users.noreply.github.com>
2022-05-31 16:28:49 +0300

    fix: add alternative to multimedia according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="task-element__delete-button button";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className='task-element__delete-button-img';
+    deleteButtonImg.alt = 'delete task button';
     deleteButton.appendChild(deleteButtonImg);
 
 
